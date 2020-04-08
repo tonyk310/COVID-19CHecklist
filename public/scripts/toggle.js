4623d2182c9a84257ebd3514583c2c1a7382e89e
Mac <mac@Tones-MacBook-Pro.local>
2020-04-08 14:05:06 -0700

    wrote the js code to toggle the display for card__info-container

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -1,6 +1,22 @@
 
 
 function toggle() {
-	var listItemP = document.getElementById('list-item-p');
-	listItemP.classList.toggle('toggle');
+	
+	var clickToDisplayArray = document.getElementsByClassName('click-to-toggle-display');
+
+	for (var i = 0; i < clickToDisplayArray.length; i++) {
+		clickToDisplayArray[i].addEventListener('click', function(e) {
+			// var event = e;
+			// console.log(event);
+			// var target = e.target;
+			// console.log(target.nextElementSibling);
+
+
+			var nextElementSibling = e.target.nextElementSibling;
+			// nextElementSibling.style.display = 'block';
+			nextElementSibling.classList.toggle('toggle');
+		});
+	}
 }
+
+toggle();
